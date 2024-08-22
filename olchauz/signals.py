5cf61a129ae9d3a371aa6a69756b8ef9066895c4
sarvar <sarvar7084295@mail.ru>
2024-08-23 01:41:37 +0500

    Clear Signals on delete

--- olchauz/signals.py
+++ olchauz/signals.py
@@ -1,8 +1,9 @@
 from django.conf import settings
+from django.core.cache import cache
 from django.db.models.signals import post_save, post_delete
 from django.dispatch import receiver
 from rest_framework.authtoken.models import Token
-from django.core.cache import cache
+
 from olchauz.models import Product
 
 
@@ -29,3 +30,19 @@ def invalidate_product_cache_on_delete(sender, instance, **kwargs):
     # Optionally, invalidate cache for any related lists or pages
     cache.delete('product_list')
 
+
+"""Cache Tozalash uchun Signal"""
+
+
+@receiver(post_save, sender=Product)
+def clear_cache_on_save(sender, instance, **kwargs):
+    cache_key = f'product_detail_{instance.slug}'
+    cache.delete(cache_key)
+    cache.delete('product_list')
+
+
+@receiver(post_delete, sender=Product)
+def clear_cache_on_delete(sender, instance, **kwargs):
+    cache_key = f'product_detail_{instance.slug}'
+    cache.delete(cache_key)
+    cache.delete('product_list'
